c24cacee24dfe02c15661f51bb1f5463f5356bde
Rubén <rpedcar0608@g.educaand.es>
2022-05-17 21:46:56 +0200

    Ahora sí que funciona

--- src/main/java/com/mycompany/git/HolaMundo.java
+++ src/main/java/com/mycompany/git/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Segundo Intento");
+        System.out.println("Ahora sí funciona");
     }
     
 }
